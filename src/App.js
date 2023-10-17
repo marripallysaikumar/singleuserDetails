bc544916b2e447baf169b4b14fdc26c640224feb
saikumarmarripally <saikumarmarripally38@gmail.com>
2023-10-17 08:53:23 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import UserProfile from './components/UserProfile/index'
 
-const userDetails = {
-  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-  name: 'Rahul',
-  role: 'software developer',
-}
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Rahul',
+    role: 'software developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'sita',
+    role: 'web developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'laxmi',
+    role: 'front-end developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'ravan',
+    role: 'back-end developer',
+  },
+]
 
-const App = () => <UserProfile userDetails={userDetails} />
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">User List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
 
 export default App
--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -5,13 +5,13 @@ const UserProfile = props => {
   const {imageUrl, name, role} = userDetails
 
   return (
-    <div className="user-card-container">
+    <li className="user-card-container">
       <img alt="sita" src={imageUrl} className="avatar" />
       <div className="user-details-container">
         <h1 className="user-name"> {name}</h1>
         <p className="user-designation">{role}</p>
       </div>
-    </div>
+    </li>
   )
 }
 
